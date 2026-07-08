e531bf88ace46fba3e9d0e7495d4acfe06707476
Sammeta01 <sammetaadithya2005@gmail.com>
2026-07-08 13:04:32 +0530

    Update solution.js

--- javascript/solution.js
+++ javascript/solution.js
@@ -1,8 +1,1 @@
-const fs = require('fs');
-
-const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/);
-
-const a = parseInt(input[0]);
-const b = parseInt(input[1]);
-
-console.log(a + b);
+// Write your code here
