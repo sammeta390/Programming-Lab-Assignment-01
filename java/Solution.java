be2e5df66c71bda5d23d3754ecba53780a9ee173
sammeta390 <22951a1241@iare.ac.in>
2026-07-08 13:25:25 +0530

    Add input handling and sum calculation

--- java/Solution.java
+++ java/Solution.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        // Write your code here
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
 
     }
 }
